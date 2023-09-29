f4e907882b1376eb47ebbbbac7c8ed694982afbd
u_alex90 <u_alex90@mail.ru>
2023-09-30 00:11:53 +1000

    Crate pages

--- catalog/views.py
+++ catalog/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
