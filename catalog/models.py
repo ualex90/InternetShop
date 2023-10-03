619728c53144fda1d14d811bdfc3208c5526276b
u_alex90 <u_alex90@mail.ru>
2023-10-03 23:56:41 +1000

    create product, category models

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='описание')
+    category_description = models.TextField(verbose_name='описание')
 
     def __str__(self):
         return f'{self.category_name}'
@@ -11,3 +11,13 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Наименование')
+    product_description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(verbose_name='Превью')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    prise = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
+    date_of_create = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    date_of_update = models.DateField(auto_now=True, verbose_name='Дата изменения')
--- config/settings.py
+++ config/settings.py
@@ -134,4 +134,4 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = Path(BASE_DIR, 'media')
