af33bf7f049d1af54e18f4d49446f23a57f6f9f4
u_alex90 <u_alex90@mail.ru>
2023-10-03 22:09:04 +1000

    remove FileManager

--- catalog/views.py
+++ catalog/views.py
@@ -3,12 +3,6 @@ from datetime import datetime as dt
 
 from django.shortcuts import render
 
-from utils.file_manager import FileManager
-from config.settings import BASE_DIR
-
-# Create your views here.
-message_file = FileManager(Path(BASE_DIR, 'fixtures'), 'message_box.json')
-
 
 def index(request):
     return render(request, 'catalog/index.html')
@@ -23,5 +17,4 @@ def contacts(request):
         # формирование сообщения
         time = dt.now()
         message = {str(time.isoformat()): {'name': name, 'phone': phone, 'message': message}}
-        message_file.update_file(message)
     return render(request, 'catalog/contacts.html')
