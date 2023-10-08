44896e06c022b87a53c61d95981e47aa5bf9c44b
u_alex90 <u_alex90@mail.ru>
2023-10-08 13:56:07 +1000

    remake migrations

--- catalog/models.py
+++ catalog/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100, verbose_name='Наименование')
-    category_description = models.TextField(verbose_name='описание')
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='описание')
 
     def __str__(self):
-        return f'{self.category_name}'
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'категория'
@@ -14,8 +14,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=100, verbose_name='Наименование')
-    product_description = models.TextField(verbose_name='Описание')
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
     image = models.ImageField(verbose_name='Превью')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
@@ -23,7 +23,7 @@ class Product(models.Model):
     updated_at = models.DateField(auto_now=True, verbose_name='Дата изменения')
 
     def __str__(self):
-        return f'{self.product_name} {self.price}'
+        return f'{self.name} {self.price}'
 
     class Meta:
         verbose_name = 'продукт'
