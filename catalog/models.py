ddb46831fc589c57157fa41dfe029bd15e65fc8b
u_alex90 <u_alex90@mail.ru>
2023-10-08 15:18:22 +1000

    mod product model

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(verbose_name='Превью')
+    image = models.ImageField(verbose_name='Превью', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
