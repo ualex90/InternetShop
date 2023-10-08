5c845450514e2823d1fbf9a7b8668eb3cb7b440e
u_alex90 <u_alex90@mail.ru>
2023-10-08 10:32:39 +1000

    create namespace

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
 from catalog.views import index, contacts
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts),
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
 ]
