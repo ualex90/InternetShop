1bb6fe5f5ff52aad603eee39aab8f144a89fe1cc
u_alex90 <u_alex90@mail.ru>
2023-10-14 09:16:27 +1000

    Create post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug', blank=True, null=True)
+    body = models.TextField(verbose_name='содержимое')
+    image = models.ImageField(verbose_name='превью', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(verbose_name='опубликовано')
+    view_count = models.IntegerField(verbose_name='количество просмотров')
+
+    def __str__(self):
+        return f'{self.created_at} {self.title}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
