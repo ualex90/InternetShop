3a63494dc6b4f67a8fb0150944e9fcaebb991e2f
u_alex90 <u_alex90@mail.ru>
2023-10-28 12:19:50 +1000

    mod models

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from catalog.models import NULLABLE
+
 
 class Post(models.Model):
     title = models.CharField(max_length=150, verbose_name='заголовок')
-    slug = models.CharField(max_length=150, verbose_name='slug', blank=True, null=True)
+    slug = models.CharField(**NULLABLE, max_length=150, verbose_name='slug')
     body = models.TextField(verbose_name='содержимое')
-    image = models.ImageField(verbose_name='превью', blank=True, null=True)
+    image = models.ImageField(**NULLABLE, verbose_name='превью')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     is_published = models.BooleanField(default=True, verbose_name='опубликовано')
     views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
